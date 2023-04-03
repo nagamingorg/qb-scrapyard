5eb8599d90d480655757221c1b62888ffa2a4674
Squad 5 <nago.thegloriouswalrus@gmail.com>
2023-04-02 20:59:37 -0500

    Scrapyard updates

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Scrapyard'
-version '1.0.0'
+version '1.2.0'
 
 shared_script {
     '@PolyZone/client.lua',
@@ -10,7 +10,8 @@ shared_script {
     '@PolyZone/ComboZone.lua',
     'config.lua',
     '@qb-core/shared/locale.lua',
-    'locales/en.lua', -- Change to the language you want
+    'locales/en.lua',
+    'locales/*.lua'
 }
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
